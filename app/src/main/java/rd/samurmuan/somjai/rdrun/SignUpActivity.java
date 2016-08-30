d89465b5f7b72767093bc1f60ab16557b370c2f7
somjai <somjai.sa@rd.go.th>
2016-08-30 10:19:37 +0700

    bind widget finsh

--- app/src/main/java/rd/samurmuan/somjai/rdrun/SignUpActivity.java
+++ app/src/main/java/rd/samurmuan/somjai/rdrun/SignUpActivity.java
@@ -2,12 +2,41 @@ package rd.samurmuan.somjai.rdrun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class SignUpActivity extends AppCompatActivity {
+//Explicit การประกาศตัวแปร
+    //ประกอบด้วย 3 อย่าง
+    //-access การเข้าถึง public private /type ประเภทข้อมูล /name
+    private EditText nameEditText,surnameEditText,userEditText, passwordEditText;
+    private RadioGroup radioGroup;
+    private RadioButton avata1RadioButton,avata2RadioButton,avata3RadioButton,avata4RadioButton, avata5RadioButton;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
-}
+        //bind or Initial Widget คือ การผูกความสัมพันธ์ระหว่างตัวแปร และ Widget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        surnameEditText = (EditText) findViewById(R.id.editText2);
+        userEditText = (EditText) findViewById(R.id.editText3);
+        passwordEditText = (EditText) findViewById(R.id.editText4);
+        radioGroup = (RadioGroup) findViewById(R.id.ragAvata);
+        avata1RadioButton = (RadioButton) findViewById(R.id.radioButton);
+        avata2RadioButton = (RadioButton) findViewById(R.id.radioButton2);
+        avata3RadioButton = (RadioButton) findViewById(R.id.radioButton3);
+        avata4RadioButton = (RadioButton) findViewById(R.id.radioButton4);
+        avata5RadioButton = (RadioButton) findViewById(R.id.radioButton5);
+
+    }//main method
+    public void clickSignUpSign(View view) {
+
+    }//clickSign
+}//main class
+
+//alt+11 hind tab
+//shift+ctrl+enter close complement
+//ctrl+space เติมคำ
+//alt+enter ใส่ แคทส
