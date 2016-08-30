593f021bdef5376b05a810a43e4e1c2909c7be3a
somjai <somjai.sa@rd.go.th>
2016-08-30 16:11:22 +0700

    finish for 30-08-2016

---
+++ app/src/main/java/rd/samurmuan/somjai/rdrun/MyConstant.java
@@ -0,0 +1,15 @@
+package rd.samurmuan.somjai.rdrun;
+
+/**
+ * Created by user on 8/30/2016.
+ */
+public class MyConstant {
+//explicit
+    private int[] avataInts = new int[]{R.drawable.bird48,
+            R.drawable.doremon48, R.drawable.kon48,
+            R.drawable.nobita48, R.drawable.rat48};
+  //กด alt+Insert
+    public int[] getAvataInts() {
+        return avataInts;
+    }
+}//Main Class
--- app/src/main/java/rd/samurmuan/somjai/rdrun/SignUpActivity.java
+++ app/src/main/java/rd/samurmuan/somjai/rdrun/SignUpActivity.java
@@ -1,11 +1,25 @@
 package rd.samurmuan.somjai.rdrun;
 
+import android.app.AlertDialog;
+import android.content.ContentProviderOperation;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+import java.nio.channels.Channels;
 
 public class SignUpActivity extends AppCompatActivity {
 //Explicit การประกาศตัวแปร
@@ -15,6 +29,8 @@ public class SignUpActivity extends AppCompatActivity {
     private RadioGroup radioGroup;
     private RadioButton avata1RadioButton,avata2RadioButton,avata3RadioButton,avata4RadioButton, avata5RadioButton;
     private String nameString,surnameString,userString,passwordString,avataString;//วิธีพิมพ์ surnameกดctrl+space
+    private static final String urlPHP ="http://swiftcodingthai.com/rd/add_user_somjai.php";//connect database
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
@@ -30,6 +46,32 @@ public class SignUpActivity extends AppCompatActivity {
         avata4RadioButton = (RadioButton) findViewById(R.id.radioButton4);
         avata5RadioButton = (RadioButton) findViewById(R.id.radioButton5);
 
+        //radio Controller
+        //new แล้ว ctrl+space
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+                switch (i) {
+                    case R.id.radioButton:
+                        avataString = "0";
+
+                        break;
+                    case R.id.radioButton2:
+                        avataString = "1";
+                        break;
+                    case R.id.radioButton3:
+                        avataString = "2";
+                        break;
+                    case R.id.radioButton4:
+                        avataString = "3";
+                        break;
+                    case R.id.radioButton5:
+                        avataString = "4";
+                        break;
+                }
+            }
+        });
     }//main method
     public void clickSignUpSign(View view) {
         //get value from edit text
@@ -43,9 +85,91 @@ public class SignUpActivity extends AppCompatActivity {
             MyAlert myAlert = new MyAlert();
             myAlert.myDialog(this,R.drawable.doremon48,"มีช่องว่าง",
                     "กรุณากรอกช่องว่าง คะ");
+        } else if (checkChoose()) {//เอา cursor วางใน () alt+enter สร้าง method อัตโนมัริ
+
+            //true ===> have choose
+            confirmValue();//เอา cursor วางใน () alt+enter สร้าง method อัตโนมัริ
+
+        } else {
+            //false ==> Non Choose
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this,R.drawable.nobita48,"ยังไม่เลือก Avata",
+                    "กรุณาเลือก Avata ก่อน คะ");
         }
 
     }//clickSign
+
+    private void confirmValue() {
+        MyConstant myConstant = new MyConstant();
+        int[] avataInts = myConstant.getAvataInts();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setIcon(avataInts[Integer.parseInt(avataString)]);
+        builder.setTitle("โปรดตรวจสอบข้อมูล");
+        builder.setMessage("Name = " + nameString +"\n"+// \n ขึ้นบันทัดใหม่
+                "Surname = "+surnameString+"\n"+
+                "User = "+userString+"\n"+
+                "Password = "+ passwordString);
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                uploadvalueToServer();
+
+            }
+        });
+        builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {//หลัง new กด ctrl+spacebar
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                uploadvalueToServer();
+                dialogInterface.dismiss();
+            }
+
+        });
+        builder.show();
+       }//confirmValue
+
+    private void uploadvalueToServer() {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()//รวมข้อมูลแล้วส่ง
+                .add("isAdd", "true")
+                .add("Name", nameString)
+                .add("Surname", surnameString)
+                .add("User", userString)
+                .add("Password", passwordString)
+                .add("Avata", avataString)
+                .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url(urlPHP).post(requestBody).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {// new กด ctrl + Space
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                finish();
+            }
+        });
+
+    }//upload
+
+
+    private boolean checkChoose() {
+        boolean result = false;
+        if (avata1RadioButton.isChecked() ||
+                avata2RadioButton.isChecked()||
+                avata3RadioButton.isChecked()||
+                avata4RadioButton.isChecked()||
+                avata5RadioButton.isChecked()) {
+
+            result = true;
+        }
+        return result;
+    }
 
     private boolean checkSpace() {
         boolean result =false;
