472ad57e9c48adc7d908d8f069c7cda6cb862c62
somjai <somjai.sa@rd.go.th>
2016-08-31 13:20:10 +0700

    load image from server finish

--- app/src/main/java/rd/samurmuan/somjai/rdrun/MainActivity.java
+++ app/src/main/java/rd/samurmuan/somjai/rdrun/MainActivity.java
@@ -4,14 +4,37 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+    //Explicit
+    private ImageView imageView;
+    private EditText userEditText, passwordEditText;//type auto user=ctrl+space
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //bind widget
+        imageView = (ImageView) findViewById(R.id.imageView6);
+        userEditText = (EditText) findViewById(R.id.editText5);
+        passwordEditText = (EditText) findViewById(R.id.editText6);// type auto Alt+Enter ===cast auto // shift+ctrl+Enter === close complement
+
+        //load Image rom Server
+        Picasso.with(this).load("http://swiftcodingthai.com/rd/Image/rd_logo.png")
+                .resize(150,150).into(imageView);
+
+
     }// Main Method นี่คือ เมธอด
+
+    public void clickSingInMain(View view) {
+
+    }//clickSingnIn
     // Get Event from Click Button
     public void clickSignUpMain(View view) {
         startActivity(new Intent(MainActivity.this,SignUpActivity.class));
